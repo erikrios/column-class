13b1873e1765de02d6abefe7473f673c6aebbdcb
erikrios <erikriosetiawan49@gmail.com>
2019-12-26 10:45:30 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Demo Flutter',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: HomePage(),
+      );
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text('Demo Column'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                child: Text('Button 1'),
+                onPressed: () {},
+              ),
+              Container(
+                width: 10,
+              ),
+              RaisedButton(
+                child: Text('Button 2'),
+                onPressed: () {},
+              ),
+              Container(
+                width: 10,
+              ),
+              RaisedButton(
+                child: Text('Button 3'),
+                onPressed: () {},
+              ),
+              Container(
+                width: 10,
+              ),
+              RaisedButton(
+                child: Text('Button 4'),
+                onPressed: () {},
+              )
+            ],
+          ),
+        ),
+      );
+}
